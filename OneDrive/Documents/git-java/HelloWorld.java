8c017430dacb4e5f3791c198687b10ef6869bd52
Ty Willis <gwillis1@stu.jsu.edu>
2023-08-28 21:54:44 -0500

    Added f2()

--- OneDrive/Documents/git-java/HelloWorld.java
+++ OneDrive/Documents/git-java/HelloWorld.java
@@ -5,9 +5,9 @@ Class1 c1 = new Class1();
 Class2 c2 = new Class2();
 System.out.println("Hello, World!");
 }
-private void f1() {
-// Placeholder method (v1.0)
-}
+private void f2() {
+    // Placeholder method (v2.0)
+    }
 }
 
 
